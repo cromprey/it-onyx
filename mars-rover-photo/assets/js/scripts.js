47f15779012628bb2ece510fefbfc8899a0bdeee
cromprey <cromprey@ualr.edu>
2023-04-17 04:27:34 +0000

    working on assignment

--- mars-rover-photo/assets/js/scripts.js
+++ mars-rover-photo/assets/js/scripts.js
@@ -1,19 +1,4 @@
-function myFunction() {
-    var pictureDate;
-    if(document.getElementById("curiosity").checked){
-        document.getElementById("picturedate").value="2012-08-06";
-    }
-
-    if(document.getElementById("opportunity").checked){
-        document.getElementById("picturedate").value="2004-01-26";
-    }
-
-    if(document.getElementById("spirit").checked){
-        document.getElementById("picturedate").value="2012-01-05";
-    }
-}
-
-async function showPhotos() {
+async function getPhotos1() {
     "use strict";
 
     // Get a reference to the form - Use the ID of the form
@@ -25,53 +10,93 @@ async function showPhotos() {
     // If all of the form elements are valid, the get the form values
     if (form.valid()) {
         
-        // Get the value associated with the operator that was checked.
-        var rover;
-        if(document.getElementById("curiosity").checked){
-            rover = document.getElementById("curiosity").value;
-        }
-
-        if(document.getElementById("opportunity").checked){
-            rover = document.getElementById("opportunity").value;
-        }
-
-        if(document.getElementById("spirit").checked){
-            rover = document.getElementById("spirit").value;
-        }
-
-        var pictureDate = document.getElementById("pictureDate").value;
+        var photoDate = document.getElementById("photoDate").value;
         var apiKey = "fwB9HJgg1OfidGGwazfRpJQHOcmgaAjdv12vajqQ";
+
+        var roverChoice;
+        if(document.getElementById("Curiosity").checked){
+            rover = document.getElementById("Curiosity").value;
+        }
+
+        if(document.getElementById("oOportunity").checked){
+            rover = document.getElementById("Opportunity").value;
+        }
+
+        if(document.getElementById("Spirit").checked){
+            rover = document.getElementById("Spirit").value;
+        }
 
         /* https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY */
         // URL for AJAX Call
-        var myURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + pictureDate + "&api_key=DEMO_KEY" + apiKey;
 
-        let pictureObject = await fetch(myURL);
-        let result = await pictureObject.text();
-        let msg = JSON.parse(Result);
-        document.getElementById("numberofphotos").innerHTML = msg.photos.length + "pictures found"
-            if(numberofphotos > 25)
-            {numberofphotos = 25}
+        var myURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverChoice + "/photos?earth_date=" + photoDate + "&api_key=api_key" + apiKey;
+        var myMethod = "GET";
 
-            for (i = 0; i < 25; i++) {
-                // Note how we construct the name for image1, image2, etc...this sets the image source
-                document.getElementById("image" + i).src = msg.photos[i].img_src;
-                /* do something to set the tool tip = msg.photos[i].camera.full_name; */
-                document.getElementById("image" + i).style.visibility = "visible";
-                document.getElementById("image" + i).title = msg.photos[i].camera.full_name;
-            }
+        $(document).ready(function() {
 
+            $.ajax({
+                method: myMethod,
+                url: myURL
+            })
 
+                .done(function(msg) {
 
+                    var numphotos = msg.photos.length;
+                    if (numphotos > 0) {
+                        for (var i = 0; i < 25; i++) {
+                            if (i < numphotos) {
+                                document.getElementById("mars1" + i).src = msg.photos[i].img_src;
+                                document.getElementById("mars1" + i).title = msg.photos[i].camera.full_name;
+                                document.getElementById("text1").innerHTML = msg.photos.length + "found photos";
+                                document.getElementById("text2").innerHTML = "Click a picture to display full size.";
+                            }
 
-        var ConvertCurrency = document.getElementById("ConvertCurrency").value;
-        var apiKey = "35eaVfKsObXpSg2O4kMLj9udr2DgVW1f"
-        var FromDate = document.getElementById("FromDate").value;
-        var ToDate = document.getElementById("ToDate").value;
-        //https://api.polygon.io/v2/aggs/ticker/C:EURUSD/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=*
-        /* URL for AJAX Call */
-        var myURL2 = "https://api.polygon.io/v2/aggs/ticker/C:" + BaseCurrency + ConvertCurrency + "/range/1/day/" + FromDate + "/" + ToDate + "?adjusted=true&sort=asc&limit=120&apiKey=" + apiKey;
+                            else{
+                                document.getElementById("rover1" + i).src = "#";
+                                document.getElementById("anchor" + i).href = "#";
+                                document.getElementById("mars1" + i).style.display = "none";
+                            }
+                        }
+                    }
+                })
+        
+
+                .fail(function (msg) {
+                    alert = "Rover not found - Status: " + msg.status;
+                });
+            });
+
     
     }
 
 }
+
+function clearform() {
+    for (var i = 0; i < 25; i++) {
+        document.getElementById("Curiousity").checked = false;
+        document.getElementById("Opportunity").checked = false;
+        document.getElementById("Spirit").checked = false;
+        document.getElementById("photoDate").value = "";
+        document.getElementById("roverError").innerHTML = "";
+        document.getElementById("photoDateError").innerHTML = "";
+        document.getElementById("rover1" + i).src = "#";
+        document.getElementById("anchor" + i).href = "";
+        document.getElementById("mars1" + i).title = "";
+        document.getElementById("text1").innerHTML = "";
+        document.getElementById("text2").innerHTML = "";
+    }
+}
+
+function getCuriosity(){
+    document.getElementById("photoDate").value= "2012-08-06";
+}
+
+function getOpportunity(){
+    document.getElementById("photoDate").value = "2004-01-26";
+}
+
+function getSpirit(){
+    document.getElementById("photoDate").value = "2004-01-05";
+}
+
+
